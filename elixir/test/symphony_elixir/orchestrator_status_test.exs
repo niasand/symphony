aaa2b78265308a6db0462b549e9b9f89b70e0c41
zhiwei.yang <tencrance@gmail.com>
2026-05-31 12:55:09 +0800

    Stabilize Elixir timing-sensitive tests

--- elixir/test/symphony_elixir/orchestrator_status_test.exs
+++ elixir/test/symphony_elixir/orchestrator_status_test.exs
@@ -941,9 +941,12 @@ defmodule SymphonyElixir.OrchestratorStatusTest do
       |> Map.put(:claimed, MapSet.put(initial_state.claimed, issue_id))
     end)
 
+    before_tick_ms = System.monotonic_time(:millisecond)
+
     send(pid, :tick)
     Process.sleep(100)
     state = :sys.get_state(pid)
+    after_state_ms = System.monotonic_time(:millisecond)
 
     refute Process.alive?(worker_pid)
     refute Map.has_key?(state.running, issue_id)
@@ -956,9 +959,8 @@ defmodule SymphonyElixir.OrchestratorStatusTest do
            } = state.retry_attempts[issue_id]
 
     assert is_integer(due_at_ms)
-    remaining_ms = due_at_ms - System.monotonic_time(:millisecond)
-    assert remaining_ms >= 9_500
-    assert remaining_ms <= 10_500
+    assert due_at_ms >= before_tick_ms + 10_000
+    assert due_at_ms <= after_state_ms + 10_000
   end
 
   test "orchestrator blocks stalled workers that are waiting on MCP elicitation" do
